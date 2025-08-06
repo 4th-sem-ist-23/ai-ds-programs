088972cd95b4fe1d7116b7e49a3bf27626fdd7b2
Mohan Kumar T <mmohan8617@gmail.com>
2025-08-06 20:11:51 +0530

    Changed cmap to autumn

--- heat_maps.py
+++ heat_maps.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 # creating a heat map
 
 data = np.random.random((12, 12))
-plt.imshow(data, cmap="winter", interpolation="nearest")
+plt.imshow(data, cmap="autumn", interpolation="nearest")
 plt.title("2-D heat map")
 plt.xlabel("Year")
 plt.ylabel("Median income")
