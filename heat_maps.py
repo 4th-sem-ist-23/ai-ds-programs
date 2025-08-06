063ba686581997dd8bc49b58e34bdc9b6e28a044
Mohan Kumar T <mmohan8617@gmail.com>
2025-08-06 20:10:27 +0530

    Added heat_maps.py

---
+++ heat_maps.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# heat map
+# creating a heat map
+
+data = np.random.random((12, 12))
+plt.imshow(data, cmap="winter", interpolation="nearest")
+plt.title("2-D heat map")
+plt.xlabel("Year")
+plt.ylabel("Median income")
+plt.show()
+
